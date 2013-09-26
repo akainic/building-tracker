b02241a618e92527cf418827545d783941ff1bad
akainic <annekainic@gmail.com>
2013-09-26 12:15:49 -0400

    Bundled gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuildingTracker::Application.config.secret_key_base = '883c1a012fb8bd94793a024cc1d87d59aa9c5c3f4d2350a8f17abd4e13295e7072157865df71fe105aedc294b41cbad8e9836bf5683a9944f0bcd63784d93367'
